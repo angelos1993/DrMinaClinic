777da058c2ba0aaa22cf78d4f3cea65bd64f1fcf
Angelos <angelos.m.reda@gmail.com>
2018-03-19 20:08:28 +0200

    Adding the master form  (FrmMaster)

---
+++ DrMinaClinic/DrMinaClinic/PL/Forms/FrmMaster.cs
@@ -0,0 +1,44 @@
+﻿using DevComponents.DotNetBar;
+using System.Windows.Forms;
+
+namespace DrMinaClinic.PL.Forms
+{
+    public partial class FrmMaster : Office2007Form
+    {
+        #region Constructor
+
+        public FrmMaster()
+        {
+            InitializeComponent();
+            SetupForm();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ErrorProvider ErrorProvider;
+        #endregion
+
+        #region Methods
+
+        private void SetupForm()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MinimizeBox = false;
+            MaximizeBox = false;
+            EnableGlass = false;
+            ShowInTaskbar = false;
+            Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular);
+            AutoScaleMode = AutoScaleMode.Font;
+            ErrorProvider = new ErrorProvider
+            {
+                ContainerControl = this,
+                RightToLeft = true
+            };
+        }
+
+        #endregion
+    }
+}
