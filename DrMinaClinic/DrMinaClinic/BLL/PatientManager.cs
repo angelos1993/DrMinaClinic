d2fb87b210a741bbda9588153d102410683a2f90
Angelos <angelos.m.reda@gmail.com>
2018-04-17 22:40:48 +0200

    Moving GetNextPatientId() from Utility.cs class to PatientManager.cs class

--- DrMinaClinic/DrMinaClinic/BLL/PatientManager.cs
+++ DrMinaClinic/DrMinaClinic/BLL/PatientManager.cs
@@ -1,4 +1,6 @@
-﻿using DrMinaClinic.BLL.Infrastructure;
+﻿using System;
+using DrMinaClinic.Utility;
+using DrMinaClinic.BLL.Infrastructure;
 
 namespace DrMinaClinic.BLL
 {
@@ -9,6 +11,24 @@ namespace DrMinaClinic.BLL
         #endregion
 
         #region Methods
+
+        public static string GetNextPatientId(string lastPatientId)
+        {
+            var today = DateTime.Now;
+            if (!lastPatientId.IsNullOrEmptyOrWhiteSpace())
+            {
+                var datePartOfLastPatientIdString = lastPatientId.Substring(0, 8);
+                if (int.TryParse(datePartOfLastPatientIdString.Substring(0, 4), out int year) &&
+                    int.TryParse(datePartOfLastPatientIdString.Substring(4, 2), out int month) &&
+                    int.TryParse(datePartOfLastPatientIdString.Substring(6, 2), out int day) &&
+                    today.Date == new DateTime(year, month, day))
+                {
+                    var lastNumber = int.Parse(lastPatientId.Substring(8));
+                    return $"{datePartOfLastPatientIdString}{lastNumber + 1}";
+                }
+            }
+            return $"{today.Year:0000}{today.Month:00}{today.Day:00}1";
+        }
 
         #endregion
     }
