952c2ac108d4c30f437252d567635cff5b26bce3
Angelos <angelos.m.reda@gmail.com>
2018-05-20 22:50:18 +0200

    replacing the normal date picker control by the one of DevCompenent -_-

--- DrMinaClinic/DrMinaClinic/PL/Forms/FrmPregnancyActualBirthDate.Designer.cs
+++ DrMinaClinic/DrMinaClinic/PL/Forms/FrmPregnancyActualBirthDate.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.labelX1 = new DevComponents.DotNetBar.LabelX();
-            this.dtActualDate = new System.Windows.Forms.DateTimePicker();
             this.btnOK = new DevComponents.DotNetBar.ButtonX();
             this.btnCancel = new DevComponents.DotNetBar.ButtonX();
             this.chkCloseWithoutDate = new DevComponents.DotNetBar.Controls.CheckBoxX();
+            this.dtActualDate = new DevComponents.Editors.DateTimeAdv.DateTimeInput();
             ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtActualDate)).BeginInit();
             this.SuspendLayout();
             // 
             // labelX1
@@ -49,13 +50,6 @@
             this.labelX1.TabIndex = 0;
             this.labelX1.Text = "Please select a birthdate to this pregnancy (if you want).";
             this.labelX1.TextAlignment = System.Drawing.StringAlignment.Center;
-            // 
-            // dtActualDate
-            // 
-            this.dtActualDate.Location = new System.Drawing.Point(50, 41);
-            this.dtActualDate.Name = "dtActualDate";
-            this.dtActualDate.Size = new System.Drawing.Size(238, 26);
-            this.dtActualDate.TabIndex = 1;
             // 
             // btnOK
             // 
@@ -92,7 +86,7 @@
             // 
             this.chkCloseWithoutDate.BackgroundStyle.Class = "";
             this.chkCloseWithoutDate.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.chkCloseWithoutDate.Location = new System.Drawing.Point(294, 41);
+            this.chkCloseWithoutDate.Location = new System.Drawing.Point(275, 41);
             this.chkCloseWithoutDate.Name = "chkCloseWithoutDate";
             this.chkCloseWithoutDate.Size = new System.Drawing.Size(197, 23);
             this.chkCloseWithoutDate.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
@@ -100,21 +94,72 @@
             this.chkCloseWithoutDate.Text = "Close without date";
             this.chkCloseWithoutDate.CheckedChanged += new System.EventHandler(this.chkCloseWithoutDate_CheckedChanged);
             // 
+            // dtActualDate
+            // 
+            // 
+            // 
+            // 
+            this.dtActualDate.BackgroundStyle.Class = "DateTimeInputBackground";
+            this.dtActualDate.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dtActualDate.ButtonDropDown.Shortcut = DevComponents.DotNetBar.eShortcut.AltDown;
+            this.dtActualDate.ButtonDropDown.Visible = true;
+            this.dtActualDate.Location = new System.Drawing.Point(69, 41);
+            // 
+            // 
+            // 
+            this.dtActualDate.MonthCalendar.AnnuallyMarkedDates = new System.DateTime[0];
+            // 
+            // 
+            // 
+            this.dtActualDate.MonthCalendar.BackgroundStyle.BackColor = System.Drawing.SystemColors.Window;
+            this.dtActualDate.MonthCalendar.BackgroundStyle.Class = "";
+            this.dtActualDate.MonthCalendar.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dtActualDate.MonthCalendar.ClearButtonVisible = true;
+            // 
+            // 
+            // 
+            this.dtActualDate.MonthCalendar.CommandsBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground2;
+            this.dtActualDate.MonthCalendar.CommandsBackgroundStyle.BackColorGradientAngle = 90;
+            this.dtActualDate.MonthCalendar.CommandsBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.dtActualDate.MonthCalendar.CommandsBackgroundStyle.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.dtActualDate.MonthCalendar.CommandsBackgroundStyle.BorderTopColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarDockedBorder;
+            this.dtActualDate.MonthCalendar.CommandsBackgroundStyle.BorderTopWidth = 1;
+            this.dtActualDate.MonthCalendar.CommandsBackgroundStyle.Class = "";
+            this.dtActualDate.MonthCalendar.CommandsBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dtActualDate.MonthCalendar.DisplayMonth = new System.DateTime(2018, 5, 1, 0, 0, 0, 0);
+            this.dtActualDate.MonthCalendar.MarkedDates = new System.DateTime[0];
+            this.dtActualDate.MonthCalendar.MonthlyMarkedDates = new System.DateTime[0];
+            // 
+            // 
+            // 
+            this.dtActualDate.MonthCalendar.NavigationBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.dtActualDate.MonthCalendar.NavigationBackgroundStyle.BackColorGradientAngle = 90;
+            this.dtActualDate.MonthCalendar.NavigationBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.dtActualDate.MonthCalendar.NavigationBackgroundStyle.Class = "";
+            this.dtActualDate.MonthCalendar.NavigationBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dtActualDate.MonthCalendar.TodayButtonVisible = true;
+            this.dtActualDate.MonthCalendar.WeeklyMarkedDays = new System.DayOfWeek[0];
+            this.dtActualDate.Name = "dtActualDate";
+            this.dtActualDate.Size = new System.Drawing.Size(200, 26);
+            this.dtActualDate.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.dtActualDate.TabIndex = 27;
+            // 
             // FrmPregnancyActualBirthDate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(540, 119);
+            this.Controls.Add(this.dtActualDate);
             this.Controls.Add(this.chkCloseWithoutDate);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
-            this.Controls.Add(this.dtActualDate);
             this.Controls.Add(this.labelX1);
             this.DoubleBuffered = true;
             this.Name = "FrmPregnancyActualBirthDate";
             this.Text = "Close Pregnancy";
             this.Load += new System.EventHandler(this.FrmPregnancyActualBirthDate_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtActualDate)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -122,9 +167,9 @@
         #endregion
 
         private DevComponents.DotNetBar.LabelX labelX1;
-        private System.Windows.Forms.DateTimePicker dtActualDate;
         private DevComponents.DotNetBar.ButtonX btnOK;
         private DevComponents.DotNetBar.ButtonX btnCancel;
         private DevComponents.DotNetBar.Controls.CheckBoxX chkCloseWithoutDate;
+        private DevComponents.Editors.DateTimeAdv.DateTimeInput dtActualDate;
     }
 }
